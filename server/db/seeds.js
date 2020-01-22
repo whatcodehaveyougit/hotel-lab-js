b115bcf598110f0227c487a23765e065c76cca99
whatcodehaveyougit <siggywatt@yahoo.com>
2020-01-22 11:30:52 +0000

    Setup done '

---
+++ server/db/seeds.js
@@ -0,0 +1,15 @@
+use hotel;
+db.dropDatabase();
+
+db.hotel.insertMany([
+  {
+    name: "Jimmy",
+    email: "jimmy@jimmy.com",
+    checkedIn: true
+  },
+  {
+    name: "bobby",
+    email: "bobby@jimmy.com",
+    checkedIn: false
+  }
+])
